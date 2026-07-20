d54427271df94b7f9f56873576e74375cc7cf834
Ishu Kansal <87827342+Ishu-Kansal@users.noreply.github.com>
2026-07-20 11:34:08 -0400

    Add: insert-new-interval - submission-0

---
+++ Data Structures & Algorithms/insert-new-interval/submission-0.py
@@ -0,0 +1,48 @@
+class Solution:
+    
+    def isOverlap(self, interval1, interval2):
+
+        if(interval1[1] < interval2[0]):
+            return False
+        if(interval2[1] < interval1[0]):
+            return False
+        return True
+
+        # second begins before first ends
+        # -----------------
+        #     ---------
+        if(interval1[1] >= interval2[0]):
+            return True
+        # first begins before second ends
+        #      -----
+        # --------------
+        if(interval1[0] <= interval2[1]):
+            return True
+        return False
+    
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        newList = []
+        i = 0
+        while(i < len(intervals) and intervals[i][1] < newInterval[0]):
+            newList.append(intervals[i])
+            i += 1
+
+        if(i == len(intervals)):
+            newList.append(newInterval)
+            return newList
+        
+        while(i < len(intervals) and self.isOverlap(intervals[i], newInterval)):
+            newInterval[0] = min(intervals[i][0], newInterval[0])
+            newInterval[1] = max(intervals[i][1], newInterval[1])
+            i += 1
+        
+        newList.append(newInterval)
+
+        while(i < len(intervals)):
+            newList.append(intervals[i])
+            i += 1
+        return newList
+
+
+
+
