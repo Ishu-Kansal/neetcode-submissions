1df283bcd021efb2241d960ec8ffea3cc1398ffb
Ishu Kansal <87827342+Ishu-Kansal@users.noreply.github.com>
2026-08-23 02:31:33 -0400

    Add: search-for-word - submission-8

---
+++ Data Structures & Algorithms/search-for-word/submission-8.py
@@ -0,0 +1,34 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        n = len(board)
+        m = len(board[0])
+
+
+        def traverse(ltr, currI, currJ, path) -> bool:
+            if(ltr == len(word)):
+                return True
+            
+            if(currI < 0 or currI >= n or currJ < 0 or currJ >= m or 
+            word[ltr] != board[currI][currJ] or (currI, currJ) in path):
+                return False
+
+            path.add((currI, currJ))
+            found = (traverse(ltr+1, currI+1, currJ, path) or
+                traverse(ltr+1, currI-1, currJ, path) or 
+                traverse(ltr+1, currI, currJ+1, path) or
+                traverse(ltr+1, currI, currJ-1, path))
+
+            path.remove((currI, currJ))
+            return found
+        
+        for i in range(n):
+            for j in range(m):
+                path = set()
+                isFound = traverse(0, i, j, path)
+                if(isFound):
+                    return True
+        
+        return False
+
+
+
