687f22128f871dfcb806fd7df771c27c94164365
Ishu Kansal <87827342+Ishu-Kansal@users.noreply.github.com>
2026-08-29 01:30:00 -0400

    Add: minimum-window-with-characters - submission-7

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-7.py
@@ -0,0 +1,48 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        inWindow = {}
+        tSize = {}
+
+        for ch in t:
+            inWindow[ch] = 0
+            tSize[ch] = 1 + tSize.get(ch, 0)
+
+        need = len(tSize)
+        have = 0
+
+        left = 0
+        right = 0
+        minLeft = float('-inf')
+        minRight = 0
+
+        while right < len(s):
+            currChar = s[right]
+            
+            if currChar in tSize:
+                inWindow[currChar] += 1
+                if inWindow[currChar] == tSize[currChar]:
+                    have += 1
+                
+                currLeftCh = s[left]
+                
+                while left < right:
+                    if currLeftCh not in tSize:
+                        left += 1
+                        currLeftCh = s[left]
+                        continue
+                    
+                    if inWindow[currLeftCh] > tSize[currLeftCh]:
+                        inWindow[currLeftCh] -= 1
+                        left += 1
+                        currLeftCh = s[left]
+                    else:
+                        break
+                if have == need and (right - left) < (minRight - minLeft):
+                    minRight = right
+                    minLeft = left
+
+            right += 1
+        
+        if minLeft == float('-inf'):
+            return ""
+        return s[minLeft:minRight+1]
