c67b3872b08d8432deec62e8297a3bacd2a839d3
Ishu Kansal <87827342+Ishu-Kansal@users.noreply.github.com>
2026-09-23 14:43:46 -0400

    Add: palindrome-number - submission-1

---
+++ Data Structures & Algorithms/palindrome-number/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if(x < 0):
+            return False
+        
+        newNum = 0
+        temp = x
+        power = 1
+        while(temp > 0):
+            newNum *= 10
+            newNum += temp % 10
+            temp //= 10
+        
+        return (newNum == x)
